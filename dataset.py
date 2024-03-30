f3e1ba8a6b63b63a597a29bd1009443b08cfe83c
yu2888 <97295265+yu2888@users.noreply.github.com>
2024-03-30 18:45:04 +0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,18 @@
+import requests
+import zipfile
+import io
+import os
+
+def download_and_extract_zip(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        zip_file = zipfile.ZipFile(io.BytesIO(response.content))
+        zip_file.extractall()
+        zip_file.close()
+        print("Zip file downloaded and extracted successfully!")
+    else:
+        print("Failed to download the zip file.")
+
+# Usage example
+repo_url = 'https://github.com/MWiechmann/enron_spam_data/blob/master/enron_spam_data.zip'
+download_and_extract_zip(repo_url)
